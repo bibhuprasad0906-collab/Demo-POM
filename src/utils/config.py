77143f2fc7847ce8b3a854fd2157ea5dde6f65ee
bibhuprasad0906-collab <bibhuprasad0906@gmail.com>
2026-01-20 20:57:11 +0530

    Update src/utils/config.py with fixed code

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,7 +6,7 @@ Supports baseUrl, browser, headless, timeouts.
 import os
 
 class Config:
-    BASE_URL = os.environ.get("BASE_URL", "http://localhost:8080/login")
+    BASE_URL = os.environ.get("BASE_URL", "http://localhost:8080")
     BROWSER = os.environ.get("BROWSER", "chrome")
     HEADLESS = os.environ.get("HEADLESS", "true").lower() == "true"
     TIMEOUT = int(os.environ.get("TIMEOUT", "10"))
