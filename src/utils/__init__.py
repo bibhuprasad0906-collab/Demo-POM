dbc0cbc3d7b87cf1a4c59d1642ebe69d4fba919d
bibhuprasad0906-collab <bibhuprasad0906@gmail.com>
2026-01-20 21:23:37 +0530

    Update src/utils/__init__.py with fixed code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,42 @@
-"""Utils package for configuration, driver factory, and custom exceptions."""
+"""Utilities package for test automation framework.
+
+Provides configuration management, driver factory, custom exceptions,
+and other utility functions for test automation.
+"""
+
+from src.utils.config import Config
+from src.utils.driver_factory import DriverFactory, get_driver
+from src.utils.exceptions import (
+    AutomationFrameworkException,
+    ElementNotFoundError,
+    ElementNotInteractableError,
+    LoginFailedError,
+    AccountLockedError,
+    PageLoadTimeoutError,
+    ConfigurationError,
+    DataValidationError,
+    ScreenshotError,
+    BrowserError,
+    TestDataNotFoundError,
+    PerformanceThresholdExceededError,
+    sanitize_error_message
+)
+
+__all__ = [
+    "Config",
+    "DriverFactory",
+    "get_driver",
+    "AutomationFrameworkException",
+    "ElementNotFoundError",
+    "ElementNotInteractableError",
+    "LoginFailedError",
+    "AccountLockedError",
+    "PageLoadTimeoutError",
+    "ConfigurationError",
+    "DataValidationError",
+    "ScreenshotError",
+    "BrowserError",
+    "TestDataNotFoundError",
+    "PerformanceThresholdExceededError",
+    "sanitize_error_message"
+]
