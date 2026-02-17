a9f2fbcc8d2ad50e1e488fab65ed6282e24f34f7
bibhuprasad0906-collab <bibhuprasad0906@gmail.com>
2026-02-17 16:04:11 +0530

    Update src/pages/__init__.py with fixed code

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -1,8 +1,4 @@
-"""Page objects package for test automation framework.
-
-Contains all page object classes implementing the Page Object Model (POM)
-pattern for clean separation of test logic and UI interactions.
-"""
+"""Page objects package."""
 
 from src.pages.base_page import BasePage
 from src.pages.login_page import LoginPage
