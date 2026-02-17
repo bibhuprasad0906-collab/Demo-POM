a394bccbe67b8759f111ad1858255fb88a31daac
bibhuprasad0906-collab <bibhuprasad0906@gmail.com>
2026-02-17 16:04:05 +0530

    Update src/__init__.py with fixed code

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,4 @@
-"""Source package for test automation framework.
-
-This package contains all page objects, utilities, and helper modules
-for the Selenium pytest automation framework.
-"""
+"""Source package for page objects and utilities."""
 
 __version__ = "1.0.0"
 __author__ = "QA Automation Team"
-__all__ = ["pages", "utils"]
